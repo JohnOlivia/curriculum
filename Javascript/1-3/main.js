2237de45c4a3502fb1f7ddbf0e9465af1f4cb8e1
Jun <jey_bzns@ymail.ne.jp>
2023-06-20 08:52:31 +0900

    理解度チェック３　Js
    課題

---
+++ Javascript/1-3/main.js
@@ -0,0 +1,84 @@
+// let num1 = 10;
+// let num2 = 4;
+// let result;
+
+// result = num1 + num2;
+// console.log(result);
+
+// result = num1 - num2;
+// console.log(result);
+
+// result = num1 * num2;
+// console.log(result);
+
+// result = num1 / num2;
+// console.log(result);
+
+// result = num1 % num2;
+// console.log(result);
+
+// console.log('Hello '+'World');
+
+// let lastName = '田中';
+// let firstName = '一浪';
+// let fullName = lastName + firstName;
+
+// console.log(fullName);
+
+// let x = 10;
+// x++;
+// console.log(x);
+// let y = 10;
+// y--;
+// console.log(y);
+
+// let score = 0;
+// if(score >= 70){
+//   console.log('合格');
+// } else if(score === 0){
+//   console.log('0点はまずいです。');
+// }else {
+//   console.log('不合格')
+// }
+
+// let a = 15;
+// if (a >= 10 && a <= 20) {
+//   console.log(a);
+// }
+
+// let a = 10;
+// let b = 15;
+
+// if (a % 2 === 0) {
+//   console.log(a);
+// }
+// if (b % 2 !== 0) {
+//   console.log(b);
+// }
+
+// WORK
+// No1
+let a = 10 != 10; 
+console.log('変数aは10ではない');
+let b = 10 
+if (b >= 10 && b < 20 && b % 2 === 0) {
+  console.log('変数bは10以上20未満、かつ偶数である');
+}
+
+
+// No2
+let x = 10;
+if(x >= 10 && x <= 20) {
+  console.log('成功です。');
+} else {
+  console.log('失敗です。');
+}
+
+// No3
+let y = 0;
+
+if (y % 2 === 0){
+  console.log('偶数です');
+}else if (y % 2 !== 0){
+  console.log('奇数です');
+}
