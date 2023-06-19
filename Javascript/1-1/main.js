a768adb5c4835e17a54bbb206bdda000631464c1
Jun <jey_bzns@ymail.ne.jp>
2023-06-19 21:53:31 +0900

    理解度チェック Js1-1
    課題1-1

---
+++ Javascript/1-1/main.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+console.log(65536);
+console.log(10 + 5);
+console.log(10 * 5);
+
+console.log('10 + 8');
+console.log(10 + 8);
+console.log('20割る3の余りは');
+console.log(20 % 3);
