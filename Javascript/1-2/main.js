fb13ce3ce7ee3657c50a1db6600d0abb63d9fb2c
Jun <jey_bzns@ymail.ne.jp>
2023-06-20 07:57:09 +0900

    理解度チェック　Js1-2
    Java Script初級　課題

---
+++ Javascript/1-2/main.js
@@ -0,0 +1,18 @@
+// let name = "池田";
+// console.log(name + "くん");
+// let number = 10;
+// console.log(number * 3);
+
+// console.log(`${name}くんは${number}月生まれです`);
+
+// const price = 100;
+// console.log(price * 2);
+
+// price = 500;
+// console.log(price * 2);
+
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
