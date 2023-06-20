9ffa1216c9712bdececcbe2ab767510aee97ec39
Jun <jey_bzns@ymail.ne.jp>
2023-06-20 12:01:39 +0900

    理解度チェック6 Js
    配列

---
+++ Javascript/1-6/main.js
@@ -0,0 +1,24 @@
+// let human = {
+//   name:'Yamada',
+//   height:170,
+//   weight:60,
+//   gender:'男',
+//   age:30,
+// };
+
+// console.log(human.name);
+// console.log(human['name']);
+
+// Work
+const scores = [10, 15, 20, 25];
+console.log(scores);
+console.log(scores[0]+'は偶数です');
+console.log(scores[2]+'は偶数です');
+
+let car = {
+  gass: 20.5,
+  num: 1234,
+}
+console.log(car);
+console.log('ガソリンは' + car.gass + 'です。');
+console.log('ナンバーは' + car.num + 'です。');
