a55bbe0673cc11ab3d7ecb06dc6553b8ffa71e19
Jun <jey_bzns@ymail.ne.jp>
2023-07-05 14:54:11 +0900

    ルーレット課題

--- Javascript/Check2/main.js
+++ Javascript/Check2/main.js
@@ -1,18 +1,11 @@
 "use strict";
 //宣言
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-// console.log(array);
 let randomNum;
 let interval;
 
-// const start = document.querySelector("#start");
-// const stop = document.querySelector("#stop");
-// const reset = document.querySelector("#reset");
-
 //jQuery
 $(function () {
-  let coloredCount = 0;
-  // $("#start").click(start_btn);
   $("#start").on("click", start_btn);
   $("#stop").on("click", stop_btn);
   $("#reset").on("click", reset_btn);
@@ -31,20 +24,22 @@ $(function () {
     $("td").removeClass("target");
     //randomNumだと0~15の値しか反応しないため＋１を追加する
     $("#td" + (randomNum + 1)).addClass("target");
-
   }
   //ストップボタンを押すと発生
   function stop_btn() {
     clearInterval(interval);
     $(".target").addClass("stop");
-    // array.splice(randomNum, 1);
-
     $("#start, #reset").prop("disabled", false);
     $("#stop").prop("disabled", true);
     if (array.length === 0) {
       $("#start").prop("disabled", false);
     }
+    //すべてのtd要素に色がついたらスタートボタンとストップボタンを押せない仕様にした
+    if ($("td.stay").length === $("td").length) {
+      $("#stop, #start").prop("disabled", true);
+    }
   }
+
   // //背景色を消す
   function reset_btn() {
     clearInterval(interval);
