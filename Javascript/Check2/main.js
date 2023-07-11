7d84606f1db5ed8a823fdbf00ae2e72fb5d16202
Jun <jey_bzns@ymail.ne.jp>
2023-07-11 14:33:35 +0900

    特別課題　7.11

--- Javascript/Check2/main.js
+++ Javascript/Check2/main.js
@@ -1,52 +1,62 @@
 "use strict";
 //宣言
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-let randomNum;
-let interval;
+{
+  let randomNum;
+  let interval;
 
-//jQuery
-$(function () {
-  $("#start").on("click", start_btn);
-  $("#stop").on("click", stop_btn);
-  $("#reset").on("click", reset_btn);
+  //jQuery
+  $(function () {
+    $("#start").on("click", start_btn);
+    $("#stop").on("click", stop_btn);
+    $("#reset").on("click", reset_btn);
 
-  //スタートボタンを押すと発生
-  function start_btn() {
-    interval = setInterval(roulette, 50);
-    $(".stop").addClass("stay").remove("stop");
-    //ボタンを押したり、押せなかったり
-    $("#start").prop("disabled", true);
-    $("#stop").prop("disabled", false);
-  }
-  //ルーレットの動き
-  function roulette() {
-    randomNum = Math.floor(Math.random() * array.length);
-    $("td").removeClass("target");
-    //randomNumだと0~15の値しか反応しないため＋１を追加する
-    $("#td" + (randomNum + 1)).addClass("target");
-  }
-  //ストップボタンを押すと発生
-  function stop_btn() {
-    clearInterval(interval);
-    $(".target").addClass("stop");
-    $("#start, #reset").prop("disabled", false);
-    $("#stop").prop("disabled", true);
-    if (array.length === 0) {
+    //スタートボタンを押すと発生
+    function start_btn() {
+      interval = setInterval(roulette, 50);
+      $(".stop").addClass("stay").remove("stop");
+      //ボタンを押したり、押せなかったり
+      $("#start").prop("disabled", true);
+      $("#stop").prop("disabled", false);
+    }
+    //ルーレットの動き
+    function roulette() {
+      // // 変数randomNumにランダムでarreyの値を入れる。
+      // randomNum = Math.floor(Math.random() * array.length) + 1;
+      // $("td").removeClass("target");
+      // //randomNumだと0~15の値しか反応しないため＋１を追加する
+      // $("#td" + randomNum).addClass("target");
+
+      // .stayクラスを持つtd要素を除外して処理をスキップ
+      let removeTd = $("td:not(.stay)");
+
+      // availableTdからランダムなtdを選択して処理を実行
+      randomNum = Math.floor(Math.random() * removeTd.length);
+      let randomTd = removeTd.eq(randomNum);
+
+      $("td").removeClass("target");
+      randomTd.addClass("target");
+    }
+
+    //ストップボタンを押すと発生
+    function stop_btn() {
+      clearInterval(interval);
+
+      $(".target").addClass("stop");
+      $("#start, #reset").prop("disabled", false);
+      $("#stop").prop("disabled", true);
+
+      //すべてのtd要素に色がついたらスタートボタンとストップボタンを押せない仕様にした
+      if ($("td.stay").length === $("td").length) {
+        $("#stop, #start").prop("disabled", true);
+      }
+    }
+
+    // //背景色を消す
+    function reset_btn() {
+      clearInterval(interval);
+      $("td").removeClass();
       $("#start").prop("disabled", false);
+      $("#reset, #stop").prop("disabled", true);
     }
-    //すべてのtd要素に色がついたらスタートボタンとストップボタンを押せない仕様にした
-    if ($("td.stay").length === $("td").length) {
-      $("#stop, #start").prop("disabled", true);
-    }
-  }
-
-  // //背景色を消す
-  function reset_btn() {
-    clearInterval(interval);
-    $("td").removeClass();
-    $("#start").prop("disabled", false);
-    $("#reset, #stop").prop("disabled", true);
-
-    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-  }
-});
+  });
+}
