37169c0442e190a7ad3419767d8b3641033380a5
Jun <jey_bzns@ymail.ne.jp>
2023-08-30 14:28:27 +0900

    修正　4章チェックテスト

--- PHP/4-2/index.php
+++ PHP/4-2/index.php
@@ -1,68 +1,67 @@
-<?php 
-require_once ('./getData.php');
+<?php
+require_once('./getData.php');
 
 $date = new getData();
 $user =  $date->getUserData();
 $post =  $date->getPostData();
-$connect = connect();
+// $connect = connect();
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <title>4章チェックテスト</title>
 </head>
+
 <body>
   <div class="wrap">
     <header>
-      <img src="images/4test_logo.png" alt=""width="180" height="100">
+      <img src="images/4test_logo.png" alt="" width="180" height="100">
       <div class="header_text">
-        <h1 class="name__area">ようこそ <?php  echo $user['last_name'].$user['first_name'];?>さん</h1>
-        <h2 class="login__area">最終ログイン日: <?php echo $user['last_login'];?></h2>
+        <h1 class="name__area">ようこそ <?php echo $user['last_name'] . $user['first_name']; ?>さん</h1>
+        <h2 class="login__area">最終ログイン日: <?php echo $user['last_login']; ?></h2>
       </div>
     </header>
     <main>
-      <?php
 
-echo '<table>';
-echo '<tr>
-<th>記事ID</th>
-<th>タイトル</th>
-<th>カテゴリ</th>
-<th>本文</th>
-<th>投稿日</th>
-</tr>';
+      <table>
+        <tr>
+          <th>記事ID</th>
+          <th>タイトル</th>
+          <th>カテゴリ</th>
+          <th>本文</th>
+          <th>投稿日</th>
+        </tr>
 
-// var_dump($post);
+        <?php foreach ($post as $row) :?>
+        <tr>
 
-foreach ($post as $row) {
-  echo '<tr>';
-  echo '<td>',$row['id'],'</td>';
-  echo '<td>',$row['title'],'</td>';
-  echo '<td>';
-  if ($row['category_no'] === 1) {
-    echo '食事';
-  } elseif ($row['category_no'] === 2) {
-    echo '旅行';
-  } else {
-    echo 'その他';
-  }
-  echo '</td>';
-  echo '<td>',$row['comment'],'</td>';
-  echo '<td>',$row['created'],'</td>';
-}
-echo '</tr>';
-echo '</table>';
+        </tr>
+          <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php if ($row['category_no'] === 1) {
+              echo '食事';
+            } elseif ($row['category_no'] === 2) {
+              echo '旅行';
+            } else {
+              echo 'その他';
+            }?></td>
+            <td><?php echo $row['comment']; ?></td>
+            <td><?php echo $row['created']; ?></td>
+        </tr>
+        <?php endforeach ;?>
+        </table>
 
-?>
     </main>
     <footer>
       <p>Y&I group.inc</p>
     </footer>
   </div>
 </body>
-</html>
 
+</html>
